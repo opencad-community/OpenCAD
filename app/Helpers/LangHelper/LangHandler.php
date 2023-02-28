64b1b49863852497517ffc11eb0b6e7500b40395
Kevin Gorman <kgorman436@gmail.com>
2023-02-28 11:57:11 +0000

    Cleanup of LangHandler
    Cleanup of LangHandler to follow inline with SonarCloud

--- app/Helpers/LangHelper/LangHandler.php
+++ app/Helpers/LangHelper/LangHandler.php
@@ -8,42 +8,42 @@ use Opencad\App\Helpers\Exceptions\Language\LanguageNotSupportedException;
 class LangHandler
 {
   // Class variables
-  private static $default_instance;
-  private $language_file;
+  private static $defaultInstance;
+  private $languageFile;
   private $language;
   private static $languages = array();
 
   /**
    * Constructor for the LanguageFile class
-   * 
-   * @param string $language_code The language code to use for the language file
+   *
+   * @param string $languageCode The language code to use for the language file
    * @throws LanguageNotSupportedException If the language code is not supported
    */
-  public function __construct($language_code)
+  public function __construct($languageCode)
   {
     $languages = array('en', 'fr', 'es'); // Define a list of supported languages
 
     // If the selected language is not supported, display an error message and default to English
-    if (!in_array($language_code, $languages)) {
-      error_log("Unsupported language selected: $language_code");
+    if (!in_array($languageCode, $languages)) {
+      error_log("Unsupported language selected: $languageCode");
       throw new LanguageNotSupportedException(
-        "The language $language_code is not supported yet! Please open a request on the GitHub"
+        "The language $languageCode is not supported yet! Please open a request on the GitHub"
       );
     }
 
-    $this->language_file = __DIR__ . "/../../../core/locale/$language_code.php";
+    $this->languageFile = __DIR__ . "/../../../core/locale/$languageCode.php";
     $this->load();
   }
 
   /**
    * Loads the language file
-   * 
+   *
    * @throws LanguageFileNotFoundException If the language file is not found
    */
   public function load()
   {
     try {
-      $this->language = include $this->language_file;
+      $this->language = include $this->languageFile;
     } catch (LanguageFileNotFoundException $e) {
       error_log("Error loading language file: " . $e->getMessage());
       $this->language = array();
@@ -61,107 +61,107 @@ class LangHandler
 
   /**
    * Gets a translation string for the given key and language code
-   * 
+   *
    * @param string $key The key for the translation string
-   * @param string|null $language_code The language code to use for the translation string
+   * @param string|null $languageCode The language code to use for the translation string
    * @return string The translation string for the given key and language code
    */
-  public static function get($key, $language_code = null)
+  public static function get($key, $languageCode = null)
   {
     // If no language code is specified, check the user's preferred language
-    if (!$language_code) {
-      $language_code = isset($_COOKIE['user_language']) ? $_COOKIE['user_language'] : null;
+    if (!$languageCode) {
+      $languageCode = htmlspecialchars(isset($_COOKIE['user_language']) ? $_COOKIE['user_language'] : null);
     }
 
     // If no language code is specified or found, default to English
-    if (!$language_code) {
-      $language_code = DEFAULT_LANGUAGE;
+    if (!$languageCode) {
+      $languageCode = DEFAULT_LANGUAGE;
     }
 
-    $language_file = __DIR__ . "/../../../core/locale/$language_code.php";
+    $languageCode = htmlspecialchars($languageCode);
+    $languageFile = __DIR__ . "/../../../core/locale/$languageCode.php";
 
     // If the language file exists, get the translation string for the given key and language code
-    if (file_exists($language_file)) {
-      $language = include $language_file;
+    if (file_exists($languageFile)) {
+      $language = include $languageFile;
       if (isset($language[$key])) {
         return $language[$key];
       }
     }
 
     // If the translation string is not found in the language file, check the static $languages array
-    if (isset(self::$languages[$language_code][$key])) {
-      return self::$languages[$language_code][$key];
+    if (isset(self::$languages[$languageCode][$key])) {
+      return self::$languages[$languageCode][$key];
     }
 
-    error_log("Translation not found: $key ($language_code)");
+    error_log("Translation not found: $key ($languageCode)");
 
     return $key;
   } /**
     * Gets the default language file instance
-    * 
+    *
     * @return LangHandler The default language file instance
     */
   public static function getDefault()
   {
-    if (!isset(self::$default_instance)) {
-      self::$default_instance = new LangHandler(DEFAULT_LANGUAGE);
+    if (!isset(self::$defaultInstance)) {
+      self::$defaultInstance = new LangHandler(DEFAULT_LANGUAGE);
     }
 
-    return self::$default_instance;
+    return self::$defaultInstance;
   }
 
   /**
    * Loads a language file from a specified language code
-   * 
-   * @param string $language_code The language code to use for the language file
+   *
+   * @param string $languageCode The language code to use for the language file
    * @return array The loaded language file
    * @throws LanguageFileNotFoundException If the language file is not found
    */
-  public static function loadFromFile($language_code)
+  public static function loadFromFile($languageCode)
   {
-    $language_file = __DIR__ . "/../../../core/locale/$language_code.php";
+    $languageFile = __DIR__ . "/../../../core/locale/$languageCode.php";
 
-    if (!file_exists($language_file)) {
-      throw new LanguageFileNotFoundException("Language file not found: $language_file");
+    if (!file_exists($languageFile)) {
+      throw new LanguageFileNotFoundException("Language file not found: $languageFile");
     }
 
-    return include $language_file;
+    return include $languageFile;
   }
 
   /**
    * Adds a translation string to the static $languages array
-   * 
+   *
    * @param string $key The key for the translation string
    * @param string $value The translation string to add
-   * @param string $language_code The language code to use for the translation string
+   * @param string $languageCode The language code to use for the translation string
    */
-  public static function addTranslation($key, $value, $language_code = 'en')
+  public static function addTranslation($key, $value, $languageCode = 'en')
   {
-    if (!isset(self::$languages[$language_code])) {
-      self::$languages[$language_code] = array();
+    if (!isset(self::$languages[$languageCode])) {
+      self::$languages[$languageCode] = array();
     }
 
-    self::$languages[$language_code][$key] = $value;
+    self::$languages[$languageCode][$key] = $value;
   }
 
   /**
    * Adds a plugin's translations to the static $languages array
-   * 
+   *
    * @param string $plugin_name The name of the plugin
-   * @param string $language_code The language code to use for the translations
+   * @param string $languageCode The language code to use for the translations
    */
-  public static function addPluginTranslations($plugin_name, $language_code = 'en')
+  public static function addPluginTranslations($plugin_name, $languageCode = 'en')
   {
-    if (!isset(self::$languages[$language_code])) {
-      self::$languages[$language_code] = array();
+    if (!isset(self::$languages[$languageCode])) {
+      self::$languages[$languageCode] = array();
     }
 
-    $plugin_locale_file = __DIR__ . "/../../../plugins/$plugin_name/locale/$language_code.php";
+    $plugin_locale_file = __DIR__ . "/../../../plugins/$plugin_name/locale/$languageCode.php";
 
     if (file_exists($plugin_locale_file)) {
       $plugin_locale = include $plugin_locale_file;
-      self::$languages[$language_code] = array_merge(self::$languages[$language_code], $plugin_locale);
+      self::$languages[$languageCode] = array_merge(self::$languages[$languageCode], $plugin_locale);
     }
   }
-
 }
